ef6a6ace72ae5528a74f97aed7a25046c8e57ce1
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-07-30 16:18:47 +0200

    Convenience flag for transparent backgrounds

--- src/render.jl
+++ src/render.jl
@@ -1,6 +1,10 @@
-function render(filename::String = tempname(),
-                filetype = :PNG,
-                animation = false)
+function render(filename::String = tempname();
+                filetype=:PNG,
+                animation=false, transparent=false)
+    # https://blender.stackexchange.com/a/149715
+    render_context.film_transparent = transparent
+    transparent && (render_context.image_settings.color_mode = "RGBA")
+
     render_context.use_file_extension = true
     render_context.filepath = filename
     render_context.image_settings.file_format = string(filetype)
